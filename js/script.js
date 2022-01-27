eed50c1708882cffe562b302f5e0ceb621a2e534
Bilal05476 <ba4762313@gmail.com>
2022-01-27 13:28:51 +0500

    updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-var navbar = document.querySelector(".navbar");
+myNavbar = document.querySelector(".my-navbar");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myNavbar.style.padding = "10px 200px";
+    myNavbar.style.backdropFilter = "blur(30px)";
+  } else {
+    myNavbar.style.backdropFilter = "blur(0px)";
+    myNavbar.style.padding = "30px 200px";
+  }
+}
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
