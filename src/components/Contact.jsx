5150e7262caea773133ab8f7aa039be995e0c0c0
Bilal05476 <ba4762313@gmail.com>
2022-02-06 00:10:47 +0500

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,26 +4,39 @@ const Contact = () => {
   return (
     <div className="contact-set" id="contact">
       <div className="header">
-        <h3>Contact Us</h3>
+        <h3>Contact Me & Subscribe</h3>
         <small>contact slogan goes here now!</small>
       </div>
-      <div className="contact-section row">
-        <div className="left-section col-md-4 col-12">
-          <input type="text" placeholder="Your Name *" required />
-          <input type="email" placeholder="Your Email *" required />
-          <input type="text" placeholder="Your Phone *" required />
+      <div className="row">
+        <form className="contact-section col-md-8 col-12">
+          <div className="row">
+            <div className="left-section col-md-4 col-12">
+              <input type="text" placeholder="Your Name *" required />
+              <input type="email" placeholder="Your Email *" required />
+              <input type="text" placeholder="Your Phone *" required />
+            </div>
+            <div className="right-section col-md-7 col-12">
+              <textarea
+                rows="9"
+                // cols="20"
+                placeholder="Your Message *"
+                required
+              ></textarea>
+            </div>
+            {/* <br /> */}
+            <div className="contact-button mx-auto">
+              <button>SEND MESSAGE</button>
+            </div>
+          </div>
+        </form>
+        <div className="col-md-4 subscribe">
+          <form>
+            <input type="email" placeholder="Your Email *" required />
+            <div className="contact-button">
+              <button>SUBSCRIBE</button>
+            </div>
+          </form>
         </div>
-        <div className="right-section col-md-7 col-12">
-          <textarea
-            rows="9"
-            cols="20"
-            placeholder="Your Message *"
-            required
-          ></textarea>
-        </div>
-      </div>
-      <div className="contact-button">
-        <button>SEND MESSAGE</button>
       </div>
     </div>
   );
