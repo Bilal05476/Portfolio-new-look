ee79236dab8f0bbe90dc7697198a130ba84503d0
Bilal05476 <ba4762313@gmail.com>
2022-02-11 15:39:01 +0500

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,15 +12,34 @@ const Contact = () => {
         </small>
       </div>
       <div className="row">
-        <form className="contact-section col-md-8 col-12" data-aos="fade-in">
+        <form
+          className="contact-section col-md-8 col-12"
+          data-aos="fade-in"
+          name="contactForm"
+          method="POST"
+          action="/success/"
+        >
+          <input type="hidden" name="form-name" value="contactForm" />
+
           <div className="row">
             <div className="left-section col-md-4 col-12">
-              <input type="text" placeholder="Your Name *" required />
-              <input type="email" placeholder="Your Email *" required />
-              <input type="text" placeholder="Your Phone *" required />
+              <input id="name" type="text" placeholder="Your Name *" required />
+              <input
+                id="email"
+                type="email"
+                placeholder="Your Email *"
+                required
+              />
+              <input
+                id="phone"
+                type="text"
+                placeholder="Your Phone *"
+                required
+              />
             </div>
             <div className="right-section col-md-7 col-12">
               <textarea
+                id="message"
                 rows="9"
                 // cols="20"
                 placeholder="Your Message *"
@@ -29,12 +48,12 @@ const Contact = () => {
             </div>
             {/* <br /> */}
             <div className="contact-button mx-auto">
-              <button>SEND MESSAGE</button>
+              <button type="submit">SEND MESSAGE</button>
             </div>
           </div>
         </form>
         <div className="col-md-4 subscribe" data-aos="fade-in">
-          <form name="subscribeForm" method="POST" action="/success">
+          <form name="subscribeForm" method="POST" action="/success/">
             <input type="hidden" name="form-name" value="subscribeForm" />
             <input
               id="email"
@@ -44,7 +63,7 @@ const Contact = () => {
               required
             />
             <div className="contact-button">
-              <button>SUBSCRIBE</button>
+              <button type="submit">SUBSCRIBE</button>
             </div>
           </form>
         </div>
---
+++ src/components/success.js
@@ -0,0 +1,9 @@
+const success = () => {
+  return (
+    <div>
+      <p>Thank you</p>
+    </div>
+  );
+};
+
+export default success;
